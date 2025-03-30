3bd42eacf9e7c88d0222d5696e5f08b7b5085e33
IDONTSUDO <idontsudo@gmail.com>
2025-03-30 14:07:20 +0300

    progress

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,16 @@
 // const greeting = document.createElement('h1');
 // greeting.textContent = 'Hello, Webpack with TypeScript and CSS!';
 // document.body.appendChild(greeting);
+
+const p = document.getElementById('123')
+const p1 = document.createElement('div');
+
+// if(p != null) p.innerHTML = `<div>5</div>`
+
+
+document.getElementById('input')?.addEventListener('input', (event) => {
+    const ggggg = event as InputEvent;
+    // console.log(p.data)
+    // console.log(event);
+    p!.innerText = ggggg.data ?? '';
+})
